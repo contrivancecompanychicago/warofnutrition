c321d50eccff19ec6770a2c5b94c3d4c4b67639d
Eric Hilary Smith <erichilarysmith@outlook.com>
2019-01-12 07:29:34 -0600

    Add files via upload

---
+++ alphabets 4 diabetes.js
@@ -0,0 +1,50 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// always-run-in-app: true; icon-color: orange;
+// icon-glyph: h-square; share-sheet-inputs: plain-text, image, url, file-url;
+// Alphabets 4 Diabetes Physical Education Curriculum Workbook Modules
+// CIC University of Illinois System
+// CIC Northwestern University
+// Northshore University Health System Evanston Illinois Campus
+// War of Nutrition Game
+// COSINE
+// World of Warcraft Blizzard Entertainment
+// Twitch TV
+// Diabetic Objects Incidences Disease Environment
+var nonPlayer;
+nonPlayer;
+var nonplayerSifidious;
+nonplayerSifidious;
+var youtubeHazardousDutyTV;
+youtubeHazardousDutyTV;
+var githubGrowthCurves;
+githubGrowthCurves;
+var githubWowDiabetes;
+githubWowDiabetes;
+var githubWowDiabetesWebsite;
+githubWowDiabetesWebsite;
+var patientPediatricNoComplications;
+patientPediatricNoComplications;
+var food;
+food;
+var foodMcdata;
+foodMcdata;
+var foodKFC;
+foodKFC;
+var drinks;
+drinks;
+var foodList;
+foodList;
+var Feasts;
+Feasts;
+var feastList;
+feastList;
+var witchcraft;
+witchcraft;
+var foodspell;
+foodspell;
+var foodspell = [];
+var drinkspell;
+drinkspell;
+var drinkspell = [];
+
