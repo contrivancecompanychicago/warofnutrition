6ca705c54be3a1e0515d5b0db4048a1c7b230d57
Eric Hilary Smith <erichilarysmith@outlook.com>
2019-01-12 07:37:33 -0600

    Add files via upload

---
+++ brickcitypatch.js
@@ -0,0 +1,28 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// always-run-in-app: true; icon-color: orange;
+// icon-glyph: magic; share-sheet-inputs: plain-text, image, url, file-url;
+var nicheFenwayFarm;
+nicheFenwayFarm;
+var nicheMissionHillLabs;
+nicheMissionHillLabs;
+var gameFenwayFarm;
+gameFenwayFarm;
+var gameMissionHillLabs;
+gameMissionHillLabs;
+var resourceFenwayFarm;
+resourceFenwayFarm;
+var fenwayfarmChicken;
+fenwayfarmChicken;
+var fenwayfarmCow;
+fenwayfarmCow;
+var fenwayfarmSweetPotato;
+fenwayfarmSweetPotato;
+var fenwayfarmSoyBean;
+fenwayfarmSoyBean;
+var costleaving;
+costleaving;
+var costVisiting;
+costVisiting;
+var costSwitching;
+costSwitching;
